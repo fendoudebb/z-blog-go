2c634fb7f015e77240bd2e4a439300b5bbab025e
fendoudebb <15317832520@163.com>
2023-05-15 09:58:12 +0800

    http tool

---
+++ helper/http.go
@@ -0,0 +1,19 @@
+package helper
+
+import "net/http"
+
+func GetProto(r *http.Request) string {
+	proto := r.Header.Get("X-Forwarded-Proto")
+	if proto == "" {
+		proto = r.Header.Get("X-Forwarded-Scheme")
+	}
+	return proto
+}
+
+func GetHost(r *http.Request) string {
+	host := r.Header.Get("X-Forwarded-Host")
+	if host == "" {
+		host = r.Header.Get("Host")
+	}
+	return host
+}
