5a052d195db727e597cfc11dc1845e15da091138
fendoudebb <15317832520@163.com>
2023-06-14 09:41:23 +0800

    search api

---
+++ api/controller/search.go
@@ -0,0 +1,37 @@
+package controller
+
+import (
+	"context"
+	"net/http"
+	"strconv"
+	"time"
+	"z-blog-go/configs"
+	"z-blog-go/db/repo"
+	"z-blog-go/web"
+)
+
+func SearchHandler(w http.ResponseWriter, r *http.Request) {
+	defer repo.Record(r, time.Now())
+	keywords := r.URL.Query().Get("keywords")
+	page, err := strconv.Atoi(r.URL.Query().Get("page"))
+	if err != nil || page < 1 {
+		page = 1
+	}
+	search := configs.GetApp().Search
+	size := search.Size
+	maxlength := search.Maxlength
+	var posts []repo.Post
+	keywordsLength := len(keywords)
+	if keywordsLength > 0 {
+		if keywordsLength > maxlength {
+			keywords = string([]rune(keywords)[:maxlength])
+		}
+		posts, _ = repo.GetPostsByKeywords(search.Tsconfig, keywords, page, size)
+	}
+	r = r.WithContext(context.WithValue(r.Context(), "page", page))
+	r = r.WithContext(context.WithValue(r.Context(), "size", size))
+	r = r.WithContext(context.WithValue(r.Context(), "keywords", keywords))
+	r = r.WithContext(context.WithValue(r.Context(), "maxlength", maxlength))
+	attr := NewAttr(r, posts)
+	web.ExecuteTemplate(w, "search", attr)
+}
