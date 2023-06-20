bf3294638ceb3477bf5b90cf5c3c8c595c123527
fendoudebb <15317832520@163.com>
2023-06-20 09:11:14 +0800

    post structure adjust

--- api/controller/post.go
+++ api/controller/post.go
@@ -4,25 +4,21 @@ import (
 	"github.com/gorilla/mux"
 	"net/http"
 	"strconv"
-	"time"
 	"z-blog-go/db/repo"
 	"z-blog-go/task"
 	"z-blog-go/web"
 )
 
 func PostHandler(w http.ResponseWriter, r *http.Request) {
-	defer repo.Record(r, time.Now())
 	vars := mux.Vars(r)
 	id, _ := strconv.Atoi(vars["id"])
 	post, _ := repo.GetPost(id)
 	attr := NewAttr(r, post)
-	attr.Extra = task.WebsiteStat.RandomPost
 	web.ExecuteTemplate(w, "post", attr)
 }
 
 func PostRandomHandler(w http.ResponseWriter, r *http.Request) {
-	defer repo.Record(r, time.Now())
-	posts, _ := repo.GetRandomPosts()
-	task.WebsiteStat.RandomPost = posts
-	Ok(w, posts)
+	randomPosts, _ := repo.GetRandomPosts()
+	task.WebsiteStat.RandomPosts = randomPosts
+	Ok(w, randomPosts)
 }
