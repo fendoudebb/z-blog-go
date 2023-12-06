3cbbf55b9db0cdb39cac92fd112fd6aeddf92ba7
fendoudebb <15317832520@163.com>
2023-12-06 22:09:03 +0800

    api refactor

--- api/api.go
+++ api/api.go
@@ -1,12 +1,14 @@
 package api
 
 import (
+	"context"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"time"
 	"z-blog-go/api/controller"
-	"z-blog-go/db/repo"
+	"z-blog-go/assets"
+	"z-blog-go/task"
 )
 
 func Register(r *mux.Router) {
@@ -19,21 +21,34 @@ func Register(r *mux.Router) {
 	r.HandleFunc("/p/{id:[0-9]+}.html", controller.PostHandler).Methods(http.MethodGet)
 	r.HandleFunc("/post/random", controller.PostRandomHandler).Methods(http.MethodPost)
 	r.HandleFunc("/search.html", controller.SearchHandler).Methods(http.MethodGet)
+
+	r.HandleFunc("/admin/index", controller.AdminIndexHandler).Methods(http.MethodGet)
+	r.HandleFunc("/admin/login", controller.AdminLoginHandler).Methods(http.MethodGet)
+
+	r.PathPrefix("/assets").Handler(http.StripPrefix("/assets", http.FileServer(http.FS(assets.FS()))))
+
+	//r.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	//	//http.ServeFile(w, r, "./build/index.html")
+	//	log.Println("static not found", r.RequestURI)
+	//})
+
 	r.NotFoundHandler = http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		log.Println("not found", request.RequestURI)
+		request = request.WithContext(context.WithValue(request.Context(), "invalidReq", true))
+		//defer task.Record(request, time.Now())
 	})
 	r.MethodNotAllowedHandler = http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		log.Println("method not allowed")
+		request = request.WithContext(context.WithValue(request.Context(), "invalidReq", true))
 	})
 	r.Use(loggingMiddleware)
-	//r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.FS(assets.FS()))))
 	//r.HandleFunc("/debug/pprof/", pprof.Index)
 	//r.HandleFunc("/debug/pprof/{cmd}", pprof.Index)
 }
 
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		defer repo.Record(r, time.Now())
+		defer task.Record(r, time.Now())
 		next.ServeHTTP(w, r)
 	})
 }
