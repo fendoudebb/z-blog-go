b1bcc364021bcd3eb76ae43df5881220ad4aba06
fendoudebb <15317832520@163.com>
2023-12-11 09:12:31 +0800

    page view refactor

--- db/repo/page_view.go
+++ db/repo/page_view.go
@@ -1,15 +1,20 @@
 package repo
 
 import (
-	"fmt"
+	"log"
 	"z-blog-go/db"
 )
 
-func CountPageView() (int, error) {
+func CountPageView() int {
 	var count int
-	row := db.DB.QueryRow("select count(*) from record_page_view")
-	if err := row.Scan(&count); err != nil {
-		return count, fmt.Errorf("CountPageView: %v", err)
+	err := db.DB.QueryRow("select count(*) from record_page_view").Scan(&count)
+	if err != nil {
+		log.Println("CountPageView:", err)
 	}
-	return count, nil
+	return count
+}
+
+func InsertPageView() {
+	db.DB.Exec(`insert into %s(url, req_method, req_param, ip_id, ua, browser, browser_platform, browser_version, browser_vendor, os, os_version, referer, cost_time, source)
+values(%s, %d, %s, %s, %s, '%s', '%s', '%s', '%s', '%s', '%s', %s, %.2f, %d)`)
 }
